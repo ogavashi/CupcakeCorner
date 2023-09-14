af3044e507d04d7bf480341f6ea87bd53ff4076e
oleg.gavashi@yahoo.com <oleg.gavashi@yahoo.com>
2023-09-14 13:29:17 +0300

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Oleg Gavashi on 14.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
